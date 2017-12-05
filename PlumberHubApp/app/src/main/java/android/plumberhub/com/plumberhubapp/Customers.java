4e427349220343bba6dbbc95ee4329a36bf58017
Razvan-Ioan Nesiu <razvannesiu@gmail.com>
2017-12-04 18:46:45 -0800

    Added few customer data options

--- PlumberHubApp/app/src/main/java/android/plumberhub/com/plumberhubapp/Customers.java
+++ PlumberHubApp/app/src/main/java/android/plumberhub/com/plumberhubapp/Customers.java
@@ -1,8 +1,9 @@
 package android.plumberhub.com.plumberhubapp;
 
 import android.content.Intent;
-import android.graphics.Color;
+import android.net.Uri;
 import android.plumberhub.com.plumberhubapp.POJOs.Customer;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +11,6 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +19,12 @@ import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import static android.provider.ContactsContract.CommonDataKinds.Email.TYPE_WORK;
+import static android.provider.ContactsContract.Intents.Insert.EMAIL;
+import static android.provider.ContactsContract.Intents.Insert.EMAIL_TYPE;
+import static android.provider.ContactsContract.Intents.Insert.NAME;
+import static android.provider.ContactsContract.Intents.Insert.PHONE;
+import static android.provider.ContactsContract.Intents.Insert.PHONE_TYPE;
 
 public class Customers extends AppCompatActivity {
 
@@ -27,6 +33,40 @@ public class Customers extends AppCompatActivity {
     private ListView lvCustomers;
     private FirebaseAuth firebaseAuth;
     private Animation animScale;
+
+    private void saveContact(Customer customer){
+        Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+        intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+        intent.putExtra(EMAIL, customer.getEmail())
+                .putExtra(EMAIL_TYPE, TYPE_WORK)
+                .putExtra(PHONE, customer.getPhone())
+                .putExtra(PHONE_TYPE, TYPE_WORK)
+                .putExtra(NAME, customer.getName());
+
+        startActivity(intent);
+    }
+
+    private void sendEmail(String email, String subject, String message){
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto",email, null));
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        startActivity(Intent.createChooser(intent, "Choose an Email client:"));
+    }
+
+    private void sendText(String number, String text){
+        Uri uri = Uri.parse("smsto:" + number);
+        Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+        intent.putExtra("sms_body", text);
+        startActivity(intent);
+    }
+
+    private void callNumber(String number){
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:"+number));
+        startActivity(callIntent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +84,10 @@ public class Customers extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 v.startAnimation(animScale);
-                if(firebaseAuth.getCurrentUser() != null) {
+                if (firebaseAuth.getCurrentUser() != null) {
                     Intent intent = new Intent(Customers.this, DialogNewCustomer.class);
                     startActivity(intent);
-                }
-                else{
+                } else {
                     Toast.makeText(Customers.this, "Authentication required!", Toast.LENGTH_LONG).show();
                 }
             }
@@ -66,12 +105,50 @@ public class Customers extends AppCompatActivity {
                 TextView txtAddress = (TextView) v.findViewById(R.id.txtAddress);
                 TextView txtPhone = (TextView) v.findViewById(R.id.txtPhone);
                 TextView txtEmail = (TextView) v.findViewById(R.id.txtEmail);
+                Button btnCall = (Button) v.findViewById(R.id.btnCall);
+                Button btnSaveContact = (Button) v.findViewById(R.id.btnSaveContact);
+                Button btnSendText = (Button) v.findViewById(R.id.btnSendText);
+                Button btnSendEmail = (Button) v.findViewById(R.id.btnSendEmail);
+                final Customer customer = model;
+
+                btnCall.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        v.startAnimation(animScale);
+                        callNumber(customer.getPhone());
+                    }
+                });
+
+                btnSendText.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        v.startAnimation(animScale);
+                        sendText(customer.getPhone(), "Hi! I'm texting you because ... ");
+                    }
+                });
+
+                btnSendEmail.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        v.startAnimation(animScale);
+                        sendEmail(customer.getEmail(), "Plumber Hub",
+                                "Hi! I just wanted to inform you that ... ");
+                    }
+                });
+
+                btnSaveContact.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        v.startAnimation(animScale);
+                        saveContact(customer);
+                    }
+                });
 
                 txtName.setText(model.getName());
                 txtAddress.setText(model.getAddress());
                 txtPhone.setText(model.getPhone());
                 txtEmail.setText(model.getEmail());
-                v.setBackgroundColor(position % 2 != 0? Color.LTGRAY : Color.WHITE);
+                //v.setBackgroundColor(position % 2 != 0? Color.LTGRAY : Color.WHITE);
             }
         };
 
